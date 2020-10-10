ce78f196cba10783296825c19b4c611fe723be85
Luiz Berto <diasbertoluiz@gmail.com>
2020-10-10 11:56:17 -0300

    Adds stats fields to struct proc
    Creation time since UNIX epoch, sleeping time, runnable time
    and running time.

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,10 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint ctime;                  // Creation time since UNIX epoch
+  int stime;                   // Sleeping time
+  int retime;                  // Runnable time
+  int rutime;                  // Running time
 };
 
 // Process memory is laid out contiguously, low addresses first:
