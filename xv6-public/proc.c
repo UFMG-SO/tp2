edbefcc7251e251923bb6d53ac9258a8e73ca41d
Luiz Berto <diasbertoluiz@gmail.com>
2020-10-15 22:51:51 -0300

    Fixes wait2 syscall

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -702,14 +702,42 @@ int set_prio(int prio) {
 }
 
 int wait2(int *retime, int *rutime, int *stime) {
-  int ret = wait();
-  struct proc *p = myproc();
-  if (ret == -1 || p == 0) {
-    return -1;
-  }
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        *retime = p->retime;
+        *rutime = p->rutime;
+        *stime = p->stime;
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
 
-  *retime = p->retime;
-  *rutime = p->rutime;
-  *stime = p->stime;
-  return 0;
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
 }
